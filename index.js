1f96e90580bc11dcb78274d72243600be18ef630
Jesus Senac romo <emilio@192.168.1.5>
2022-02-05 18:30:44 -0700

    done

--- index.js
+++ index.js
@@ -2,20 +2,45 @@ let items = []
 const button = document.getElementById('btn')
 const input = document.getElementById('inputEl')
 const listed = document.getElementById('ulEL')
+const deleteButton = document.getElementById('delete-btn')
+const saveButton = document.getElementById('save-tab')
+const fromLocalStorage = JSON.parse(localStorage.getItem("items"))
+
+if(fromLocalStorage){
+    items = fromLocalStorage
+    format(items)
+}
+
+
+saveButton.addEventListener('click', function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    items.push(tabs[0].url)
+    localStorage.setItem('items', JSON.stringify(items))
+    format(items)
+    })
+    
+})
+
+deleteButton.addEventListener('dblclick', function(){
+    localStorage.clear()
+    items = []
+    format(items)
+})
 
 button.addEventListener('click', function(){
     items.push(input.value)
     input.value = ''
-    format()
+    localStorage.setItem("items", JSON.stringify(items))
+    format(items)
 })
 
-function format(){
+function format(item){
     let listItems = ''
     for(let i = 0; i< items.length; i++){
         listItems += `
         <li>
-        <a target='_blank' href='https://www.${items[i]}.com/'>
-        ${items[i]}</a>
+        <a target='_blank' href='https://www.${item[i]}.com/'>
+        ${item[i]}</a>
         </li>
         `
     }
